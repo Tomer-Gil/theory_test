3522d1c7aa7db63c0da2074666dc9ef5158b37de
Tomer Gil <tomgi859@gmail.com>
2021-06-05 15:08:01 +0300

    A few changes in the code logic. Read more in the description.
    - Added dynamic fetch of the most updated resource's id, instead of the static one in the previous version.
    - Added the ability to control the offset (which question to start fetch from) and the limit (how many questions should be fetched
    	in one time).
    	Currently the app fetches LIMIT questions from CKAN at a time, times up until the last question (CKAN's default is 100).
    	The app can be manipulated to fetch a specific number of questions by changing LIMIT to the desired amount, and set the total
    	variable to be equal to it (rather than the difference between it and the OFFSET).
    	To be implemented in the future - determine how many times to fetch LIMIT questions.
    - Renamed variables in postRequestToCkan(...) frrom lower_case_with_underscore styling to camelCase styling,
    	to follow JS naming convention (which is slightly different than the Python named convention, as appears of PEP8#naming-conventions).
    - In postRequestToCkan(...), added a few more keys to post_data
    - Removed unnecessary comments of old console.log(...)'s
    - Removed the unnecessary handleApiResponse(...) function

--- app.js
+++ app.js
@@ -5,21 +5,10 @@ const cheerio = require("cheerio");
 const express = require('express');
 const app = express();
 
-function handleApiResponse(api_response, i=0) {
-    api_response = JSON.parse(api_response);
-    const total = api_response.result.total;
-    var next_offset = url.parse(api_response.result._links.next, true).query.offset;
-    console.log(total);
-    if(i == 0) {
-        questions = questions.concat(["tomer"]);
-    }
-    console.log(questions);
-}
-
-function postRequestToCkan(limit, i, offset=0) {
+function postRequestToCkan(id, offset=0, limit=100, i=0) {
     return new Promise(function(resolve, reject) {
-        var post_data = JSON.stringify({
-            resource_id: "bf7cb748-f220-474b-a4d5-2d59f93db28d",
+        var postData = JSON.stringify({
+            resource_id: id,
             limit: limit,
             offset: offset,
             fields: ["_id", "title2", "description4", "category"].join(),
@@ -34,36 +23,36 @@ function postRequestToCkan(limit, i, offset=0) {
             headers: {
                 // "Content-Type": "application/x-www-form-urlencoded",
                 "Content-Type": "application/json",
-                "Content-Length": Buffer.byteLength(post_data),
+                "Content-Length": Buffer.byteLength(postData),
                 "Transfer-Encoding": "chunked"
             }
         }
     
         var callback = function(response) {
-            var api_response = [];
+            var apiResponse = [];
             response.on("data", function(chunk) {
-                api_response = api_response.concat(chunk);
+                apiResponse = apiResponse.concat(chunk);
             });
             response.on("end", function() {
-                // handleApiResponse(api_response);
-                api_response = Buffer.concat(api_response);
-                api_response = JSON.parse(api_response);
-                const total = api_response.result.total;
-                let resourceId = api_response.result.resource_id;
+                // handleApiResponse(apiResponse);
+                apiResponse = Buffer.concat(apiResponse);
+                apiResponse = JSON.parse(apiResponse);
+                const total = apiResponse.result.total;
+                let resourceId = apiResponse.result.resource_id;
                 // Logically it should be used by the next .then() object in the chain,
                     // but I can't figure out how to chain multiple .then() objects.
-                var nextOffset = url.parse(api_response.result._links.next, true).query.offset;
+                var nextOffset = url.parse(apiResponse.result._links.next, true).query.offset;
                 if(i === 0) {
-                    resolve([total, api_response]);
+                    resolve([resourceId, total, apiResponse]);
                 } else {
-                    resolve([api_response]);
+                    resolve([apiResponse]);
                 }
             });
         }
     
-        const request_to_ckan = https.request(options, callback);
-        request_to_ckan.write(post_data);
-        request_to_ckan.end();
+        const requestToCkan = https.request(options, callback);
+        requestToCkan.write(postData);
+        requestToCkan.end();
     });
 }
 
@@ -71,83 +60,135 @@ app.set('view engine', 'ejs');
 app.use("/public", express.static("static"));
 
 
-app.get("/", function(req, res) {
-    // console.log(path.join(__dirname,"public"));
-    // console.log(__dirname + "/public")
-    const LIMIT = 100;  // API default
-    var i = 0;
-    postRequestToCkan(LIMIT, 0).then(function(success) {
-        [total, apiResponse] = success;
-        var apiResponses = [];
-        var i;
-        for(i = 1; i < parseInt(total / LIMIT); i++) {
-            apiResponses = apiResponses.concat(postRequestToCkan(LIMIT, i, offset=LIMIT*i));
-        }
-        apiResponses = apiResponses.concat(postRequestToCkan(total % i, i, LIMIT*i));
-        Promise.all([[apiResponse]].concat(apiResponses)).then((values) => {
-            // Values - Array of arrays of one object - the API response' objects
-
-            // questions_objects - array of the API response' objects
-            let questions_objects = values.map(function(inner_arr) {
-                return inner_arr[0];
+app.get('/', function(req, res) {
+    const OFFSET = 0; // API default is 0
+    const LIMIT = 20;  // API default is 100
+    new Promise(function(resolve, reject) {
+        let queryStrings = {
+            q: "tqhe"
+        };
+        let options = {
+            host: "data.gov.il",
+            path: ["/api/3/action/package_search", new URLSearchParams(queryStrings).toString()].join("?")
+        };
+        let callback = function(response) {
+            let apiResponse = [];
+            response.on("data", function(chunk) {
+                apiResponse = apiResponse.concat(chunk);
             });
-
-            // Extract the records out of the whole API response objects.
-            // questions_records - array of arrays of 100 records.
-            let questions_records = questions_objects.map(function(questions_object) {
-                return questions_object.result.records;
-            });
-
-            // Concat the arrays.
-            // questions_records - array of 1802 (or future total) objects.
-            questions_records = questions_records.reduce(function(questionsArray, nextQuestionsArray) {
-                return questionsArray.concat(nextQuestionsArray);
-            });
-
-            questions_records.forEach(function(records_object, i) {
-                var $ = cheerio.load(records_object.description4);
-                var descriptions = [];
-                var correctAnswer = -1;
-                $("li > span").each(function(j, element) {
-                    descriptions = descriptions.concat([$(element).text()]);
-                    if("id" in element.attribs) {
-                        correctAnswer = j;
-                    }
-                });
-                questions_records[i].descriptions = descriptions;
-                questions_records[i].correctAnswer = correctAnswer;
-
-                questions_records[i].optionalImage = {};
-                questions_records[i].optionalImage.source = $("img").attr("src");
-                questions_records[i].optionalImage.alt = $("img").attr("alt");
-                questions_records[i].optionalImage.title = $("img").attr("title");
-
-                var vehiclesTypes = $("span:nth-of-type(2)").text().match(/[A-Z]|[0-9]|[A-Z][0-9]/g);
-                questions_records[i].vehiclesTypes = vehiclesTypes;
-
-                var title2 = questions_records[i].title2.match(/([0-9]{1,4})(\.\s)(.*)/);
-                var questionNumber = title2[1];
-                var questionTitle = title2[3];
-                questions_records[i].questionNumber = parseInt(questionNumber);
-                questions_records[i].questionTitle = questionTitle;
-                // Optional regex for the title itself - 
-                // /(?<=[0-9]\.\s)(.|\s)*/
-
-                // Optional, for convenience manners - 
-                // delete questions_records[i].description4;
-            });
-/*             questions_records.sort(function(a, b) {
-                return a.questionNumber - b.questionNumber;
-            }); */
-            questions_records.forEach(function(question_record, index) {
-                if(question_record.vehiclesTypes === null) {
-                    console.log(index + " vehiclesTypes is null.");
+            response.on("end", function() {
+                apiResponse = Buffer.concat(apiResponse);
+                apiResponse = JSON.parse(apiResponse);
+                let count = apiResponse.result.count;
+                if (count < 0) {
+                    reject(`Invalid number of packages (datasets) found for the query ${query} - less than 0.`);
+                } else if(count === 0) {
+                    reject(`No packages(datasets) with the query ${query} found.`);
+                } else if (count === 1) {
+                    resolve(apiResponse.result.results[0]);
+                } else {
+                    reject(`Too many packages (datasets) found for the query ${query} - greater than 1.`);
                 }
             });
-            res.render('original', {questions: questions_records});
-            console.log("Debug");
+        };
+        https.request(options, callback).end();
+    }).then(function(success) {
+        // Success holds one package object.
+        // Sanitize if necessary
+        let resources = success.resources;
+        resources = resources.filter(function(resource) {
+            return resource.datastore_active;
         });
-    }, function(error) {});
+        let latestResource = resources.reduce(function(currentLatest, currentDate){
+            return (currentLatest.last_modified > currentDate.last_modified ? currentLatest : currentDate);
+        });
+        let id = latestResource.id;
+        postRequestToCkan(id, OFFSET, LIMIT).then(function(success) {
+            [resourceId, total, apiResponse] = success;
+            total = LIMIT;
+            // total = total - OFFSET;
+            var apiResponses = [];
+            for(var i = 1; i < parseInt(total / LIMIT); i++) {
+                apiResponses = apiResponses.concat(postRequestToCkan(resourceId, LIMIT*i + OFFSET, LIMIT, i));
+            }
+            apiResponses = apiResponses.concat(postRequestToCkan(resourceId, LIMIT*i + OFFSET, total % LIMIT, i));
+            Promise.all([[apiResponse]].concat(apiResponses)).then((values) => {
+                // Values - Array of arrays of one object - the API response' objects
+
+                const isSort = false;
+
+                // questions_objects - array of the API response' objects
+                let questions_objects = values.map(function(inner_arr) {
+                    return inner_arr[0];
+                });
+
+                // Extract the records out of the whole API response objects.
+                // questions_records - array of arrays of 100 records.
+                let questions_records = questions_objects.map(function(questions_object) {
+                    return questions_object.result.records;
+                });
+
+                // Concat the arrays.
+                // questions_records - array of 1802 (or future total) objects.
+                questions_records = questions_records.reduce(function(questionsArray, nextQuestionsArray) {
+                    return questionsArray.concat(nextQuestionsArray);
+                });
+
+                function parseField(field) {
+                    return (field !== undefined ? decodeURI(field.trim()).replace(/^"/, "").replace(/"$/, "") : undefined);
+                }
+
+                questions_records.forEach(function(records_object, i) {
+                    var $ = cheerio.load(records_object.description4);
+
+                    var title2 = questions_records[i].title2.match(/([0-9]{1,4})(\.\s)(.*)/);
+                    var questionNumber = parseInt(parseField(title2[1]));
+                    var questionTitle = parseField(title2[3]);
+                    questions_records[i].questionNumber = questionNumber;
+                    questions_records[i].questionTitle = questionTitle;
+                    
+                    var descriptions = [];
+                    var correctAnswer = -1;
+                    $("li > span").each(function(j, element) {
+                        descriptions = descriptions.concat([parseField($(element).text())]);
+                        if("id" in element.attribs) {
+                            correctAnswer = j;
+                        }
+                    });
+                    questions_records[i].descriptions = descriptions;
+                    questions_records[i].correctAnswer = correctAnswer;
+
+                    questions_records[i].optionalImage = {};
+                    questions_records[i].optionalImage.source = parseField($("img").attr("src"));
+                    questions_records[i].optionalImage.alt = parseField($("img").attr("alt"));
+                    questions_records[i].optionalImage.title = parseField($("img").attr("title"));
+
+                    var vehiclesTypes = parseField($("span:nth-of-type(2)").text());
+                    if(/[а-яА-ЯЁё]/.test(vehiclesTypes)) {
+                        vehiclesTypes = vehiclesTypes.replace(/\u0412/g, "B");
+                        // Implement more replacements if needed.
+                    }
+                    vehiclesTypes = vehiclesTypes.match(/[A-Z]|[0-9]|[A-Z][0-9]/g);
+                    questions_records[i].vehiclesTypes = vehiclesTypes;
+                    // Optional regex for the title itself - 
+                    // /(?<=[0-9]\.\s)(.|\s)*/
+                    // There's no question 713.
+
+                    // Optional, for convenience manners - 
+                    // delete questions_records[i].description4;
+                });
+                if(isSort) {
+                    questions_records.sort(function(a, b) {
+                        return a.questionNumber - b.questionNumber;
+                    });
+                }
+                res.render('index', {questions: questions_records});
+                console.log("Debug");
+            });
+        }, function(error) {});
+    }, function(error) {
+        console.log(error);
+    });
 });
 
 app.listen(5000);
